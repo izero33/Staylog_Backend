aad59f2a553c6f625e9b1e94a9943617f7059a13
Danpung <kkokko514@naver.com>
2025-11-01 15:57:33 +0900

    [추가] 이미지관련 클래스파일 추가

--- staylog/src/main/java/com/staylog/staylog/domain/image/service/impl/ImageServiceImpl.java
+++ staylog/src/main/java/com/staylog/staylog/domain/image/service/impl/ImageServiceImpl.java
@@ -1,7 +1,12 @@
 package com.staylog.staylog.domain.image.service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.staylog.staylog.domain.image.dto.request.ImageLoadRequest;
+import com.staylog.staylog.domain.image.dto.request.ImageUploadRequest;
+import com.staylog.staylog.domain.image.service.ImageService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +15,21 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class ImageServiceImpl {
+public class ImageServiceImpl implements ImageService{
+	
+	@Value("${file.image-location}")
+	private String filePath;
+	
+	@Override
+	public void saveImage(ImageUploadRequest uploadRes) {
+		
+		
+	}
+
+	@Override
+	public void loadImage(ImageLoadRequest loadRes) {
+		
+		
+	}
 
 }
