cbc68ab2300bf4759d003adfed8462b7df03ecc5
Danpung <kkokko514@naver.com>
2025-11-06 20:23:49 +0900

    [수정] SSE 컨트롤러 수정

--- staylog/src/main/java/com/staylog/staylog/domain/notification/controller/SseController.java
+++ staylog/src/main/java/com/staylog/staylog/domain/notification/controller/SseController.java
@@ -3,6 +3,8 @@ package com.staylog.staylog.domain.notification.controller;
 import com.staylog.staylog.domain.notification.service.SseService;
 import com.staylog.staylog.global.common.code.ErrorCode;
 import com.staylog.staylog.global.exception.BusinessException;
+import com.staylog.staylog.global.security.SecurityUser;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -31,7 +33,9 @@ public class SseController {
      */
     @Operation(summary = "클라이언트 SSE 채널 구독", description = "로그인한 사용자의 토큰을 검증하여 SSE 채널에 구독시킵니다.")
     @GetMapping(value = "/notification/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@AuthenticationPrincipal Long userId) {
+    public SseEmitter subscribe(@AuthenticationPrincipal SecurityUser user) {
+    	Long userId = user.getUserId();
+    	System.out.println("SSE 연결 시도 | userID: " + userId);
 
         if (userId == null) {
             // 로직상 필터 401를 뱉지만 만약을 대비한 방어
