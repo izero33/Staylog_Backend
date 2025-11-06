d225082b6f46f42b7d28e11eb0dcd6eb4cb626c1
Danpung <kkokko514@naver.com>
2025-11-06 18:01:48 +0900

    [추가] 이미지 삭제 관련 SQL 쿼리 및 Mapper클래스 메소드 추가

--- staylog/src/main/java/com/staylog/staylog/domain/image/mapper/ImageMapper.java
+++ staylog/src/main/java/com/staylog/staylog/domain/image/mapper/ImageMapper.java
@@ -12,17 +12,19 @@ import java.util.Map;
 @Mapper
 public interface ImageMapper {
 	void insertImage(ImageDto imageDto);
-	/**
-	 * @deprecated 동시성 문제로 더이상 사용하지 않음
-	 */
-	long selectMaxDisplayOrder(String targetType, long targetId);
-	List<ImageDto> selectImagesByTarget(String targetType, long targetId);
 	ImageDto selectProfileByUserId(String targetType, @Param("targetId") long userId);
 
-	//정나영 : 보드 아이디만(타겟 아이디)만
-	Long getNextBoardId();
-	
 	Long selectForUpdate(Map<String, Object> params);
 	void updateCounter(Map<String, Object> params);
 	void createCounter(Map<String, Object> params);
+
+	void deleteImage(long imageId);
+	void updateDisplayOrderAfterDelete(Map<String, Object> params);
+	void decrementCounter(Map<String, Object> params);
+	
+	List<ImageDto> selectImagesByTarget(String targetType, long targetId);
+
+	
+	//정나영 : 보드 아이디만(타겟 아이디)만
+	Long getNextBoardId();
 }
